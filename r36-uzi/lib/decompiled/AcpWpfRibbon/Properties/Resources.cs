3830eb0bc0e7ff2e608fd71052a5e65b312f578d
victorevargasvera-debug <victor.e.vargas.vera@gmail.com>
2026-09-14 12:04:05 -0300

    Add files via upload

---
+++ r36-uzi/lib/decompiled/AcpWpfRibbon/Properties/Resources.cs
@@ -0,0 +1,46 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DevComponents.WpfRibbon.Properties.Resources
+// Assembly: AcpWpfRibbon, Version=23.0.0.26, Culture=neutral, PublicKeyToken=d124d6ba4931c72b
+// MVID: 45A745B5-88C3-40EE-B1E5-8DBA2EACD529
+// Assembly location: F:\DECOMP\36-UZI\APXFamilyCPS1\lib_org\AcpWpfRibbon.dll
+
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+#nullable disable
+namespace DevComponents.WpfRibbon.Properties;
+
+[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+[DebuggerNonUserCode]
+[CompilerGenerated]
+internal class Resources
+{
+  private static ResourceManager resourceMan;
+  private static CultureInfo resourceCulture;
+
+  internal Resources()
+  {
+  }
+
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  internal static ResourceManager ResourceManager
+  {
+    get
+    {
+      if (DevComponents.WpfRibbon.Properties.Resources.resourceMan == null)
+        DevComponents.WpfRibbon.Properties.Resources.resourceMan = new ResourceManager("DevComponents.WpfRibbon.Properties.Resources", typeof (DevComponents.WpfRibbon.Properties.Resources).Assembly);
+      return DevComponents.WpfRibbon.Properties.Resources.resourceMan;
+    }
+  }
+
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  internal static CultureInfo Culture
+  {
+    get => DevComponents.WpfRibbon.Properties.Resources.resourceCulture;
+    set => DevComponents.WpfRibbon.Properties.Resources.resourceCulture = value;
+  }
+}
---
+++ r36-uzi/lib/decompiled/AcpWpfRibbon/Properties/Settings.cs
@@ -0,0 +1,21 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: DevComponents.WpfRibbon.Properties.Settings
+// Assembly: AcpWpfRibbon, Version=23.0.0.26, Culture=neutral, PublicKeyToken=d124d6ba4931c72b
+// MVID: 45A745B5-88C3-40EE-B1E5-8DBA2EACD529
+// Assembly location: F:\DECOMP\36-UZI\APXFamilyCPS1\lib_org\AcpWpfRibbon.dll
+
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+#nullable disable
+namespace DevComponents.WpfRibbon.Properties;
+
+[CompilerGenerated]
+[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+internal sealed class Settings : ApplicationSettingsBase
+{
+  private static Settings defaultInstance = (Settings) SettingsBase.Synchronized((SettingsBase) new Settings());
+
+  public static Settings Default => Settings.defaultInstance;
+}
